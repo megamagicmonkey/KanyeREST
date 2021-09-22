c9cac6b6a78296ba39d4f541bb79ffe4152c60aa
megamagicmonkey <megamagicmonkey@gmail.com>
2021-09-22 09:03:19 -0500

    initial commit

---
+++ KanyeREST/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeREST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest/";
+            
+
+            var swansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            //.ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+            
+            
+            for (int i = 0; i < 5; i++)
+            {
+
+                var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+                var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+                var swansonResponse = client.GetStringAsync(swansonURL).Result;
+                var swansonQuote = JArray.Parse(swansonResponse);
+
+                Console.WriteLine("The Kanye says:");
+                Console.WriteLine(kanyeQuote);
+                Console.WriteLine("The Ron retorts:");
+                Console.WriteLine(swansonQuote[0]);
+                
+
+            }
+        }
+    }
+}
